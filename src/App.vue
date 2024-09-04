99ca836aef75e018995891b16c3ef9f483caa676
FE4902 <isdn000123@gmail.com>
2024-09-05 00:16:42 +0900

    영화 정보 외부 파일로 이동

--- src/App.vue
+++ src/App.vue
@@ -25,34 +25,14 @@
 </template>
 
 <script>
+import datas from "./assets/movies";
+
 export default {
     name: "App",
     data() {
         return {
             isModal: false,
-            datas: [
-                {
-                    imgUrl: "/assets/노량.jpg",
-                    title: "노량",
-                    year: 2023,
-                    category: "액션, 드라마",
-                    like: 0,
-                },
-                {
-                    imgUrl: "/assets/아쿠아맨.jpg",
-                    title: "아쿠아맨과 로스트 킹덤",
-                    year: 2023,
-                    category: "액션, 판타지, 어드밴처",
-                    like: 0,
-                },
-                {
-                    imgUrl: "/assets/3일의휴가.jpg",
-                    title: "3일의 휴가",
-                    year: "2023",
-                    category: "판타지, 드라마",
-                    like: 0,
-                },
-            ],
+            datas: datas,
         };
     },
     methods: {

75b2c43b1d29ebf128edf034a31ef2760b263457
FE4902 <isdn000123@gmail.com>
2024-09-05 00:11:12 +0900

    v-if

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
         <h1>영화 정보</h1>
-        <div v-for="(data, i) in datas" :key="data.id">
-            <h3 class="bg-yellow">{{ data.title }}</h3>
-            <p>개봉: {{ data.year }}</p>
-            <p>장르: {{ data.category }}</p>
-            <button @click="increaseLike(i)">좋아요</button>
-            <span>{{ data.like }}</span>
+        <div class="item" v-for="(data, i) in datas" :key="data.id">
+            <figure>
+                <img :src="`${data.imgUrl}`" :alt="data.title" />
+            </figure>
+            <div class="info">
+                <h3 class="bg-yellow">{{ data.title }}</h3>
+                <p>개봉: {{ data.year }}</p>
+                <p>장르: {{ data.category }}</p>
+                <button @click="increaseLike(i)">좋아요</button>
+                <span>{{ data.like }}</span>
+                <p><button @click="isModal = true">상세보기</button></p>
+            </div>
+        </div>
+        <div class="modal" v-if="isModal">
+            <div class="inner">
+                <h3>Detail</h3>
+                <p>영화 상세정보</p>
+                <button @click="isModal = false">닫기</button>
+            </div>
         </div>
     </div>
 </template>
@@ -16,20 +29,24 @@ export default {
     name: "App",
     data() {
         return {
+            isModal: false,
             datas: [
                 {
+                    imgUrl: "/assets/노량.jpg",
                     title: "노량",
                     year: 2023,
                     category: "액션, 드라마",
                     like: 0,
                 },
                 {
+                    imgUrl: "/assets/아쿠아맨.jpg",
                     title: "아쿠아맨과 로스트 킹덤",
                     year: 2023,
                     category: "액션, 판타지, 어드밴처",
                     like: 0,
                 },
                 {
+                    imgUrl: "/assets/3일의휴가.jpg",
                     title: "3일의 휴가",
                     year: "2023",
                     category: "판타지, 드라마",
@@ -47,8 +64,69 @@ export default {
 </script>
 
 <style>
-.bg-yellow {
-    background-color: gold;
-    padding: 10px;
+* {
+    box-sizing: border-box;
+    margin: 0;
+}
+
+body {
+    max-width: 768px;
+    margin: 0 auto;
+    padding: 20px;
+}
+
+h1,
+h2,
+h3 {
+    margin-bottom: 1rem;
+}
+
+p {
+    margin-bottom: 0.5rem;
+}
+
+button {
+    margin-right: 10px;
+    margin-top: 1rem;
+}
+
+.item {
+    width: 100%;
+    border: 1px solid #ccc;
+    display: flex;
+    margin-bottom: 20px;
+    padding: 1rem;
+}
+
+.item figure {
+    width: 30%;
+    margin-right: 1rem;
+}
+
+.item img {
+    width: 100%;
+}
+
+.item .info {
+    width: 100%;
+}
+
+.modal {
+    background: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.modal .inner {
+    background: #fff;
+    width: 80%;
+    padding: 20px;
+    border-radius: 10px;
 }
 </style>

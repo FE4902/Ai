5bb80f200851ce333d4c20fe3d56d5621bfd9994
FE4902 <isdn000123@gmail.com>
2024-09-06 00:06:39 +0900

    Movies 컴포넌트

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,13 @@
 <template>
     <div>
         <Navbar />
-        <h1>영화 정보</h1>
-        <div class="item" v-for="(data, i) in datas" :key="data.id">
-            <figure>
-                <img :src="`${data.imgUrl}`" :alt="data.title" />
-            </figure>
-            <div class="info">
-                <h3 class="bg-yellow">{{ data.title }}</h3>
-                <p>개봉: {{ data.year }}</p>
-                <p>장르: {{ data.category }}</p>
-                <button @click="increaseLike(i)">좋아요</button>
-                <span>{{ data.like }}</span>
-                <p>
-                    <button
-                        @click="
-                            isModal = true;
-                            selectedMovie = i;
-                        "
-                    >
-                        상세보기
-                    </button>
-                </p>
-            </div>
-        </div>
+        <Movies
+            :datas="datas"
+            @openModal="
+                isModal = true;
+                selectedMovie = $event;
+            "
+        />
         <Modal
             :datas="datas"
             :isModal="isModal"
@@ -36,6 +20,7 @@
 <script>
 import datas from "./assets/movies";
 import Navbar from "./components/Navbar.vue";
+import Movies from "./components/Movies.vue";
 import Modal from "./components/Modal.vue";
 
 export default {
@@ -54,6 +39,7 @@ export default {
     },
     components: {
         Navbar,
+        Movies,
         Modal,
     },
 };
---
+++ src/components/Movies.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <h1>영화 정보</h1>
+        <div class="item" v-for="(data, i) in datas" :key="data.id">
+            <figure>
+                <img :src="`${data.imgUrl}`" :alt="data.title" />
+            </figure>
+            <div class="info">
+                <h3 class="bg-yellow">{{ data.title }}</h3>
+                <p>개봉: {{ data.year }}</p>
+                <p>장르: {{ data.category }}</p>
+                <button @click="increaseLike(i)">좋아요</button>
+                <span>{{ data.like }}</span>
+                <p>
+                    <button @click="$emit('openModal', i)">상세보기</button>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MoviesComponent",
+    props: {
+        datas: Array,
+    },
+};
+</script>
+
+<style>
+.container {
+    padding: 20px;
+}
+</style>

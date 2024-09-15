1c97ed2c9770787bcee38b2b210620c8335c4248
FE4902 <isdn000123@gmail.com>
2024-09-15 23:38:11 +0900

    init portfolio project

--- src/App.vue
+++ src/App.vue
@@ -1,132 +1,30 @@
-<template>
-    <div>
-        <Navbar />
-        <Event :text="text[eventTextNumber]" />
-        <SearchBar :datas="datas_temp" @searchMovie="searchMovie($event)" />
-        <p><button @click="showAllMovie">전체보기</button></p>
-        <Movies
-            :datas="datas_temp"
-            @openModal="
-                isModal = true;
-                selectedMovie = $event;
-            "
-            @increaseLike="increaseLike($event)"
-        />
-        <Modal
-            :datas="datas"
-            :isModal="isModal"
-            :selectedMovie="selectedMovie"
-            @closeModal="isModal = false"
-        />
-    </div>
-</template>
-
-<script>
-import datas from "./assets/movies";
-import Navbar from "./components/Navbar.vue";
-import Event from "./components/Event.vue";
-import Movies from "./components/Movies.vue";
-import Modal from "./components/Modal.vue";
-import SearchBar from "./components/SearchBar.vue";
-
-export default {
-    name: "App",
-    data() {
-        return {
-            isModal: false,
-            datas: datas,
-            datas_temp: [...datas],
-            selectedMovie: 0,
-            text: [
-                "넷플릭스 강렬한 운명의 드라마, 경성크리쳐",
-                "디즈니 100주년 기념작, 위시",
-                "그날, 대한민국의 운명이 바뀌었다, 서울의 봄",
-            ],
-            eventTextNumber: 0,
-        };
-    },
-    methods: {
-        increaseLike(id) {
-            // this.datas[i].like += 1;
-            this.datas.find((movie) => {
-                if (movie.id == id) {
-                    movie.like += 1;
-                }
-            });
-        },
-        searchMovie(title) {
-            this.datas_temp = this.datas.filter((movie) => {
-                return movie.title.includes(title);
-            });
-        },
-        showAllMovie() {
-            this.datas_temp = [...this.datas];
-        },
-    },
-    components: {
-        Navbar,
-        Event,
-        Movies,
-        Modal,
-        SearchBar,
-    },
-    mounted() {
-        setInterval(() => {
-            if (this.eventTextNumber == this.text.length - 1) {
-                this.eventTextNumber = 0;
-            } else {
-                this.eventTextNumber += 1;
-            }
-        }, 3000);
-    },
-};
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-* {
-    box-sizing: border-box;
-    margin: 0;
-}
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
-body {
-    max-width: 768px;
-    margin: 0 auto;
-    padding: 20px;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-h1,
-h2,
-h3 {
-    margin-bottom: 1rem;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-
-p {
-    margin-bottom: 0.5rem;
-}
-
-button {
-    margin-right: 10px;
-    margin-top: 1rem;
-}
-
-.item {
-    width: 100%;
-    border: 1px solid #ccc;
-    display: flex;
-    margin-bottom: 20px;
-    padding: 1rem;
-}
-
-.item figure {
-    width: 30%;
-    margin-right: 1rem;
-}
-
-.item img {
-    width: 100%;
-}
-
-.item .info {
-    width: 100%;
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>

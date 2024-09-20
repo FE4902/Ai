e5872dc4ecb76236676a3fa366fdb0aff5265dcf
FE4902 <isdn000123@gmail.com>
2024-09-20 23:55:54 +0900

    SearchBar 컴포넌트

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
+import SearchBar from "./components/SearchBar.vue";
 </script>
 
 <template>
     <div>
         <Navbar />
+        <SearchBar />
         <font-awesome-icon :icon="['fas', 'location-dot']" />
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import App from "./App.vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
+import {
+    faBarsStaggered,
+    faLocationDot,
+    faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBarsStaggered);
+library.add(faBarsStaggered, faLocationDot, faMagnifyingGlass);
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
